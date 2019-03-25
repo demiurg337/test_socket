f3fc29c104fca2282f93422d246c36c8ffaa13cd
demiurg337 <dmitro.gedz@gmail.com>
2019-03-25 18:35:57 +0200

    Getting of ip of some domain

--- get_ip.c
+++ get_ip.c
@@ -4,11 +4,18 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 
 int main(int argc, char *argv[]) 
 {
     struct addrinfo hints, *res, *p;
+    char * ipVersion;
+    void * address;
+    struct sockaddr_in *ipv4;
+    struct sockaddr_in6 *ipv6;
+    struct sockaddr *ipv;
+    char ip_str[INET_ADDRSTRLEN];
    
     if (argc != 2) {
         fprintf(stderr, "Need to set the hostname !");
@@ -16,4 +23,29 @@ int main(int argc, char *argv[])
     }
 
     memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = 0;
+
+    if (getaddrinfo(argv[1], NULL, &hints, &res) != 0) {
+        fprintf(stderr, "Can't get information about domain!\n");
+    }
+
+    for (p = res; p != NULL; p = p->ai_next) {
+        ipv = (struct sockaddr *) p->ai_addr;
+        //IP4
+        if (p->ai_family == AF_INET) {
+            ipVersion = "IPv4";
+            ipv4 = (struct sockaddr_in *) p->ai_addr;
+            address = &(ipv4->sin_addr);
+        //IP6
+        } else {
+            ipVersion = "IPv6";
+            ipv6 = (struct sockaddr_in6 *) p->ai_addr;
+            address = &(ipv6->sin6_addr);
+        }
+        
+        inet_ntop(p->ai_family, address, ip_str, sizeof ip_str);
+        printf("%s : %s\n\n", ip_str, ipVersion); 
+
+    }
 }
