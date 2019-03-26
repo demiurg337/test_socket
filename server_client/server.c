0c140976d8d64a81b4904d36be0a25dc2e5a6b06
demiurg337 <dmitro.gedz@gmail.com>
2019-03-26 08:05:19 +0200

    First code for server

---
+++ server_client/server.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+int main()
+{
+    struct addrinfo hints, *servinfo, *p;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    //flag for receiving connections
+    hints.ai_flags = AI_PASSIVE;
+
+}
